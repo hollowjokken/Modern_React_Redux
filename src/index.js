2ccba0b2fda20c924ffed85860ee90610aa58e04
Norbi <norbert.ronai@evozon.com>
2017-08-07 20:19:33 +0300

    git test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 /**
  * Created by norbertronai on 07-Aug-17.
  */
+import React from "react";
+import ReactDOM from 'react-dom';
+// Create new component
+//asddf
+
+const App= function () {
+    return <div>Fuck GIT! </div>;
+};
+
+ReactDOM.render(<App />, document.querySelector('.container'));
+//Take this component's generated HTML and put it on the page (in the DOM)
---
+++ src/testgit.js
@@ -0,0 +1,4 @@
+/**
+ * Created by norbertronai on 07-Aug-17.
+ */
+const gitsucks = 10;
